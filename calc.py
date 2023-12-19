7d11429adc4cd8ccea6819c34c7eafdf8f08ad57
D1lo <dban59732@gmail.com>
2023-12-20 00:41:07 +0500

    создали окно и задали ему параметры

--- calc.py
+++ calc.py
@@ -1,5 +1,11 @@
+# импортировали библиотеку tkinter и ее модули
 from tkinter import ttk
 from tkinter import Tk
 from tkinter.messagebox import showerror
+
+# создание окна приложения
+window=Tk() # связывание переменной с объектом
+window.title("Сумма чисел") # название окна
+window.geometry("300x100") # размер окна
 
 
